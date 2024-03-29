747ff9e7a0d817ffd6f1f6e7fe1056d4530a7574
GuLhe <lhermitteg@gmail.com>
2024-03-29 14:12:03 +0100

    For accessibility, the converter should also be available in written form
    (aside from phonetical which already is great and helped my shadock ass
    in many perilous situations ...)

--- shadok.hs
+++ shadok.hs
@@ -4,43 +4,43 @@ type Decimal = Int
 type ShadokNumber = String
 
 shadok :: Decimal -> ShadokNumber
-shadok 0  = "GA"
-shadok 1  = "BU"
-shadok 2  = "ZO"
-shadok 3  = "MEU"
+shadok 0  = "◯"
+shadok 1  = "–"
+shadok 2  = "⌟"
+shadok 3  = "⊿"
 shadok x  = shadok ( x `div` 4 ) ++ shadok ( x `mod` 4 )
 
 
 main = hspec $ do
 
     describe "Shadok numeric base" $ do
-        it "0, 1, 2, 3 should be GA BU ZO MEU" $ do
-             shadok 0 `shouldBe` "GA"
-             shadok 1 `shouldBe` "BU"
-             shadok 2 `shouldBe` "ZO"
-             shadok 3 `shouldBe` "MEU"
+        it "0, 1, 2, 3 should be ◯ – ⌟ ⊿" $ do
+             shadok 0 `shouldBe` "◯"
+             shadok 1 `shouldBe` "–"
+             shadok 2 `shouldBe` "⌟"
+             shadok 3 `shouldBe` "⊿"
 
 
     describe "Multiple of four" $ do
-        it "With only one GA" $ do
-            shadok 4  `shouldBe` "BUGA"
-            shadok 8  `shouldBe` "ZOGA"
-            shadok 12 `shouldBe` "MEUGA"
+        it "With only one ◯" $ do
+            shadok 4  `shouldBe` "–◯"
+            shadok 8  `shouldBe` "⌟◯"
+            shadok 12 `shouldBe` "⊿◯"
 
-        it "With multiple GA" $ do
-            shadok 16 `shouldBe` "BUGAGA"
+        it "With multiple ◯" $ do
+            shadok 16 `shouldBe` "–◯◯"
 
     describe "Any number which is not a multiple of four" $ do
         it "two shadoks verb number" $ do
-            shadok 7  `shouldBe` "BUMEU"
-            shadok 15 `shouldBe` "MEUMEU"
-            shadok 5  `shouldBe` "BUBU"
+            shadok 7  `shouldBe` "–⊿"
+            shadok 15 `shouldBe` "⊿⊿"
+            shadok 5  `shouldBe` "––"
 
         it "three shadoks verb number" $ do
-            shadok 19 `shouldBe` "BUGAMEU"
+            shadok 19 `shouldBe` "–◯⊿"
 
         it "big number" $ do
-            shadok 9999 `shouldBe` "ZOBUMEUGAGAMEUMEU"
+            shadok 9999 `shouldBe` "⌟–⊿◯◯⊿⊿"
 
         it "very big number" $ do 
-            shadok 47850 `shouldBe` "ZOMEUZOZOMEUZOZOZO"
+            shadok 47850 `shouldBe` "⌟⊿⌟⌟⊿⌟⌟⌟"
